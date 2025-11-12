2c6a17206a733aef124e8961e1d9039b5b6b21ae
richducat <richducat@gmail.com>
2025-11-11 18:32:32 -0800

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return ComponentComponent {...pageProps} />;
+}
+
+export default MyApp;
